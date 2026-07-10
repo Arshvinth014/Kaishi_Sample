67600e0830e5fdaa09fbd2f88f50e12075114f02
Arshvinth <arshvinth2004@gmail.com.com>
2026-07-10 20:07:23 +0530

    Fixed import

--- client/src/components/home/SuccessSection.tsx
+++ client/src/components/home/SuccessSection.tsx
@@ -9,6 +9,7 @@ import Shi from '../../assets/images/homePage/Shi.png';
 const SuccessSection: React.FC = () => {
     const profileImages = [Kai, Shi, Kai];
 
+    
     return (
         <div className="w-full bg-slate-300/40 py-4 px-4 sm:px-6 lg:px-8 font-sans text-slate-800 antialiased">
             <div className="max-w-7xl mx-auto space-y-6">
